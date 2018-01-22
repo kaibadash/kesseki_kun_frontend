b10a1cc15512661053c3523dfdbe2376af172abe
kaiba <kaibadash@gmail.com>
2018-01-22 10:27:13 +0900

    WIP

--- src/Auth/Auth.ts
+++ src/Auth/Auth.ts
@@ -1,16 +1,71 @@
 import * as auth0 from 'auth0-js';
+import * as SuperAgent from 'superagent';
+import { Auth0DecodedHash } from 'auth0-js';
 
 export default class Auth {
   auth = new auth0.WebAuth({
     domain: 'kessekikun.auth0.com',
     clientID: 'uO9Xrji9cmgq4RAELpj2bKxq4bsb9Etv',
-    redirectUri: 'http://localhost:3000/callback',
+    redirectUri: 'http://localhost/callback',
     audience: 'https://kessekikun.auth0.com/userinfo',
     responseType: 'token id_token',
     scope: 'openid'
   });
 
   login() {
-    this.auth.authorize();
+    this.auth.authorize({
+      responseType: 'token id_token',
+      scope: 'openid profile'
+    });
+  }
+
+  handleAuthentication() {
+    this.auth.parseHash((err, authResult) => {
+      if (authResult && authResult.accessToken && authResult.idToken) {
+        this.requestCreateServiceUser(authResult);
+        this.setSession(authResult);
+        history.replaceState(null, '/home');
+      } else if (err) {
+        history.replaceState(null, '/home');
+        console.log(err);
+      }
+    });
+  }
+
+  requestCreateServiceUser(authResult: Auth0DecodedHash) {
+    SuperAgent
+      .post('/api/v1/users/new')
+      .query({ token: authResult.accessToken! })
+      .end((err: any, res: SuperAgent.Response) => {
+      console.log('requested!');
+    });
+  }
+
+  setSession(authResult: Auth0DecodedHash) {
+    // Set the time that the access token will expire at
+    console.log('authResult', authResult);
+    let expiresAt = JSON.stringify((authResult.expiresIn! * 1000) + new Date().getTime());
+    localStorage.setItem('access_token', authResult.accessToken!);
+    localStorage.setItem('id_token', authResult.idToken!);
+    localStorage.setItem('expires_at', expiresAt);
+    // navigate to the home route
+    history.replaceState(null, '/home');
+  }
+
+  logout() {
+    // Clear access token and ID token from local storage
+    localStorage.removeItem('access_token');
+    localStorage.removeItem('id_token');
+    localStorage.removeItem('expires_at');
+    // navigate to the home route
+    history.replaceState(null, '/home');
+  }
+
+  isAuthenticated() {
+    // Check whether the current time is past the 
+    // access token's expiry time
+    let expire = localStorage.getItem('expires_at');
+    let expiresAt = JSON.parse(expire!);
+    return new Date().getTime() < expiresAt;
   }
 }
--- src/Callback.tsx
+++ src/Callback.tsx
@@ -1,7 +1,10 @@
 import * as React from 'react';
+import Auth from './Auth/Auth';
 
 class Callback extends React.Component {
   render() {
+    const auth = new Auth();
+    auth.handleAuthentication();
     return (
       <div>
         <h1>Callback.tsx</h1>
